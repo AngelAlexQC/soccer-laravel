004bf59994e0cd8026ee56faf630dd5ea259d4c2
Ángel Quiroz <aquiroz@apsposus.com>
2022-02-15 22:16:58 -0500

    Fix Function

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', function()=>{
+Route::view('/', function(){
     // Redirect to 'admin'
     return redirect()->route('admin');
 });
